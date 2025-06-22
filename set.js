10ac4aa66bccbd48e98466fcd964717d927737aa
Collie254 <colliericchie@gmail.com>
2025-06-22 18:40:54 +0300

    Update set.js

--- set.js
+++ set.js
@@ -13,7 +13,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
-    PREFIXE: process.env.PREFIX || ".",
+    PREFIXE: process.env.PREFIX || "¡",
     OWNER_NAME: process.env.OWNER_NAME || "254710772666",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
